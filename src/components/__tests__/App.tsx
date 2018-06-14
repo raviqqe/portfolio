6fdebdc89c27f38722dd557dcdf7b5da03979501
Yota Toyama <raviqqe@gmail.com>
2018-06-15 08:08:09 +0900

    Modify default files

---
+++ src/components/__tests__/App.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import App from "../App";
+
+test("Render a component", () => ReactDOM.render(<App />, document.createElement("div")));
