3b0c41e1a18e30ec86cdfb97cd722cc2a9bce5bc
Yota Toyama <raviqqe@gmail.com>
2018-06-15 08:27:57 +0900

    Use styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { injectGlobal } from "styled-components";
 
 import App from "./components/App";
+
+injectGlobal`
+    body {
+        font: 16px sans-serif;
+        width: 100vw;
+        height: 100vh;
+        padding: 0;
+        margin: 0;
+        background: black;
+    }
+
+    div#root {
+        width: 100%;
+        height: 100%;
+    }
+`;
 
 ReactDOM.render(<App />, document.getElementById("root"));
