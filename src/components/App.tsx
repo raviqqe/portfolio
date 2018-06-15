ac581f0c7dc48a0bdd3577d93f33d15da68a27eb
Yota Toyama <raviqqe@gmail.com>
2018-06-15 09:36:48 +0900

    Add domain code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 
-export default class extends React.Component {
+import { Page } from "../domain";
+
+interface IState {
+    page: Page;
+}
+
+export default class extends React.Component<{}, IState> {
     public render() {
         return <div>Hello, world!</div>;
     }
---
+++ src/domain.ts
@@ -0,0 +1,6 @@
+export enum Page {
+    Works = "works",
+    Contribution = "contribution",
+    Study = "study",
+    Story = "story",
+}
