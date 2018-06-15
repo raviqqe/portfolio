1abeec31eb39b7d1d96d632cd6757c09b202ea1f
Yota Toyama <raviqqe@gmail.com>
2018-06-16 06:49:32 +0900

    Add page buttons component

---
+++ src/components/PageButtons.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Book = require("react-icons/lib/fa/book");
+import Cubes = require("react-icons/lib/fa/cubes");
+import GitHub = require("react-icons/lib/fa/github");
+import Rocket = require("react-icons/lib/fa/rocket");
+import styled from "styled-components";
+
+import { Page } from "../domain";
+import PageButton from "./PageButton";
+
+const PageButtons = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export default class extends React.Component {
+    public render() {
+        return (
+            <PageButtons>
+                <PageButton icon={<Cubes />} page={Page.Works} />
+                <PageButton icon={<GitHub />} page={Page.Contribution} />
+                <PageButton icon={<Book />} page={Page.Study} />
+                <PageButton icon={<Rocket />} page={Page.Story} />
+            </PageButtons>
+        );
+    }
+}
