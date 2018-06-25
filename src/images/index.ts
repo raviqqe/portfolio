b62f623a175c199065cb05024da901018dc23e1c
Yota Toyama <raviqqe@gmail.com>
2018-06-26 02:48:09 +0900

    Use HTTPS for Linuxbrew image

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,2 +1,2 @@
-export const Linuxbrew = "http://linuxbrew.sh/images/linuxbrew-256x256.png";
+export const Linuxbrew = "https://linuxbrew.sh/images/linuxbrew-256x256.png";
 export const Rust = "https://www.rust-lang.org/logos/rust-logo-blk.svg";
