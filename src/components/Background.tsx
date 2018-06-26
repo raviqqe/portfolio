741b5556a85056a6fc0c24bb8445ecb0ad533b91
Yota Toyama <raviqqe@gmail.com>
2018-06-27 01:05:51 +0900

    Animate background transition

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 
 import { backgroundLightness, colors, masks, Page } from "../domain";
 
+const transitionMs = 300;
+
 const Background = styled.div<{ color: string, mask: string }>`
     position: fixed;
     left: 0;
@@ -14,22 +16,45 @@ const Background = styled.div<{ color: string, mask: string }>`
     background-color: ${({ color }) => color};
     mask-image: url("${({ mask }) => mask}");
     mask-size: cover;
-    transition: background-color 1s;
+    transition: background-color ${transitionMs}ms;
+    transition-timing-function: linear;
 `;
 
 interface IProps {
     page: Page;
 }
 
-export default class extends React.Component<IProps> {
-    public render() {
-        const { page } = this.props;
+interface IState {
+    color: string;
+    mask: string;
+}
 
-        return (
-            <Background
-                color={lighten(backgroundLightness[page], colors[page])}
-                mask={masks[page]}
-            />
-        );
+export default class This extends React.Component<IProps> {
+    private static convertPageToColor(page: Page): string {
+        return lighten(backgroundLightness[page], colors[page]);
+    }
+
+    public state: IState = {
+        color: This.convertPageToColor(Page.About),
+        mask: masks[Page.About],
+    };
+
+    public render() {
+        const { color, mask } = this.state;
+
+        return <Background color={color} mask={mask} />;
+    }
+
+    public componentDidUpdate({ page }: IProps) {
+        if (page !== this.props.page) {
+            this.setState({ color: "transparent" });
+
+            const { page } = this.props;
+
+            setTimeout(() => this.setState({
+                color: This.convertPageToColor(page),
+                mask: masks[page],
+            }), transitionMs);
+        }
     }
 }
